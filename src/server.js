96c476a50d78662476c4a7c2a59a9ebceb94a062
dvankevich <dvankevich@gmail.com>
2025-01-10 21:45:16 +0200

    add error handlers

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import cors from 'cors';
 
 import { getEnvVar } from './utils/getEnvVar.js';
 import contactsRouter from './routers/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -29,18 +31,9 @@ export const startServer = () => {
 
   app.use(contactsRouter);
 
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
+  app.use('*', notFoundHandler);
 
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
