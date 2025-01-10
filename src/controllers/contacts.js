ade85df46c82d4ba80b7bfd4cffe81ead120d200
dvankevich <dvankevich@gmail.com>
2025-01-10 21:26:03 +0200

    add wrapper

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -10,15 +10,13 @@ export const getContactsController = async (req, res) => {
   });
 };
 
-export const getContactByIdController = async (req, res) => {
+export const getContactByIdController = async (req, res, next) => {
   const { contactId } = req.params;
   const contact = await getContactById(contactId);
 
   // Відповідь, якщо не знайдено
   if (!contact) {
-    res.status(404).json({
-      message: 'Contact not found',
-    });
+    next(new Error('Contact not found'));
     return;
   }
 
