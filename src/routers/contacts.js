bf03734cb0ed93ebf00dda02ec251c28fa94def0
dvankevich <dvankevich@gmail.com>
2025-01-12 20:58:32 +0200

    add validation schemas

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,13 +7,26 @@ import {
   patchContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createContactsSchema,
+  updateContactsSchema,
+} from '../validation/contacts.js';
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-router.post('/contacts', ctrlWrapper(createContactController));
-router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.post(
+  '/contacts',
+  validateBody(createContactsSchema),
+  ctrlWrapper(createContactController),
+);
+router.patch(
+  '/contacts/:contactId',
+  validateBody(updateContactsSchema),
+  ctrlWrapper(patchContactController),
+);
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
 export default router;
